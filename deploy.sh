d09820bbe9ea132bd4263199a15b85e9fa09d682
Dave <davidajberner@gmail.com>
2016-03-25 13:19:18 +1100

    gh-pages update

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+git checkout gh-pages
+
+git reset --hard origin/master
+
+cp -r dist/* .
+
+npm run clean-source
+
+git add -A .
+
+git commit -a -m 'gh-pages update'
+
+git push origin gh-pages --force
+
+git checkout master
